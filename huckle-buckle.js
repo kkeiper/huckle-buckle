189433bc7fde2a8fac3ea9001db2af7501e64262
Kevin Keiper <kevin@kevinkeiper.com>
2014-01-07 16:00:20 -0800

    Adding everything to master

---
+++ huckle-buckle.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	var userGuess,
+		answer,
+		numberOfGuesses = 0, //declares all variables
+		randomNumber=Math.floor(Math.random()*100); //generates my random number between 0 - 100
+
+console.log("You have guessed " + userGuess, answer, numberOfGuesses, randomNumber);
+
+$("#newGame").click(function() {
+	$("#inputBox").html().
+});
+
+
+
+});
+
+
