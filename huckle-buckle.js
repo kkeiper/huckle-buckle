38d52053a74aa4191662d534add450cea1569495
Kevin Keiper <kevin@kevinkeiper.com>
2014-01-14 17:07:21 -0800

    adding final files for project

--- huckle-buckle.js
+++ huckle-buckle.js
@@ -1,30 +1,62 @@
 $(document).ready(function() {
-	var userGuess,
+	
+	//declares all variables
+	var userGuess, 
 		answer,
-		numberOfGuesses = 0, //declares all variables
+		guessCount
+
+		$("#newGame").click(function() {
+		resetGame();
+		});
+
+		$("#submit").click(function() {
+		userGuess = $("#inputBox").val();
+		console.log(userGuess);
+		checkGuess(userGuess);
+		$("#guesses p").html(userGuess);
+		});
+		
+		var resetGame = function() { 
 		randomNumber=Math.floor(Math.random()*100); //generates my random number between 0 - 100
+		console.log(randomNumber);
+		$("#inputBox").val("");
+		$("#guesses p").html("");
+		$("#message").html("Do you feel lucky?");
+		guessCount = 0;
+		};
 
-console.log("You have guessed " + userGuess, answer, numberOfGuesses, randomNumber);
+		function setCount(count) {
+		$('#guesses').text(guessCount);
+		};
 
-$("#newGame").click(function() {
-	// $("#inputBox").innerHTML;
-	// console.log($("#inputBox").innerHTML);
-	// console.log(document.getElementById("inputBox").value);
-	$("#inputBox").val("");
-	$("#guesses").html("");
-	$("#message").html("Do you feel lucky?");
-	numberOfGuesses = 0;
+	var checkGuess = function(userGuess) {
+		var difference = Math.abs(randomNumber - userGuess);
+		if (isNaN(userGuess)) {
+			$("#message").html("enter a valid number");
 
-});
+		} else if (userGuess < 1 || userGuess > 100){
+			$("#message").html("enter a number between 1 and 100");
+		} else {
+			guessCount++;
+			setCount(guessCount);
+			$("ul#guessList").append("<li>" + userGuess + "</li>");
+			$("#inputBox").val("");
 
-
-
-$("#submit").click(function() {
-	userGuess = $("#inputBox").val();
-	console.log(userGuess);
-	numberOfGuesses++;
-	console.log(numberOfGuesses);
-});
+			if (userGuess == randomNumber) {
+				$("#message").html("you won...").css("color", "black")
+			} else if (difference < 5) {
+				$("#message").html("very warm...").css("color", "red")
+			} else if (difference < 10) {
+				$("#message").html("warm...").css("color", "orange")
+			} else if (difference < 20) {
+				$("#message").html("cold...").css("color", "#00bfff")
+			} else {
+				$("#message").html("freezing...").css("color", "blue")
+				
+			}
+		}
+	};
+		resetGame();
 
 });
 
